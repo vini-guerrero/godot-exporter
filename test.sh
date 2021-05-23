ba41611da1eca6ccae8c42b2f325085e02afe685
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-23 17:42:11 -0300

    CI

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo 'GODOT_VERSION='${{ env.GODOT_VERSION }} >> .env
-echo 'GODOT_DL_SUBDIR='${{ env.GODOT_DL_SUBDIR }} >> .env
-echo 'GODOT_RELEASE='${{ env.GODOT_RELEASE }} >> .env
-
 echo "Godot Version: $GODOT_VERSION"
 echo "Godot Subdir: $GODOT_DL_SUBDIR"
 echo "Godot Release: $GODOT_RELEASE"
