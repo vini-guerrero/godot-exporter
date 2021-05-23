c25aac0fffd39b4ee77dd43b3baaa772592a47d5
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-23 17:45:21 -0300

    CI

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@
 echo "Godot Version: $GODOT_VERSION"
 echo "Godot Subdir: $GODOT_DL_SUBDIR"
 echo "Godot Release: $GODOT_RELEASE"
+echo "https://downloads.tuxfamily.org/godotengine/${GODOT_DL_SUBDIR}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip"
 echo " "
 printenv
