1489f7559de70ebfe25378e5447f121d5769f3b6
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-23 17:49:29 -0300

    CI

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-GODOT_VERSION = $GODOT_VERSION
-GODOT_DL_SUBDIR = $GODOT_DL_SUBDIR
-GODOT_RELEASE = $GODOT_RELEASE
 
 echo "Godot Version: $GODOT_VERSION"
 echo "Godot Subdir: $GODOT_DL_SUBDIR"
 echo "Godot Release: $GODOT_RELEASE"
+
+echo ${{ env.GODOT_VERSION }}
+echo ${{ env.GODOT_DL_SUBDIR }}
+echo ${{ env.GODOT_RELEASE }}
+
 echo "https://downloads.tuxfamily.org/godotengine/${GODOT_DL_SUBDIR}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip"
 echo " "
 printenv
