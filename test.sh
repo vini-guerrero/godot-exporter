5caa202665e0e6fe2cbcd69ffcbb2e33763142fc
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-23 18:58:40 -0300

    CI

--- test.sh
+++ test.sh
@@ -2,21 +2,19 @@
 
 echo "✔ Setup script triggered successfully."
 
-GODOT_VERSION = $1
-GODOT_DL_SUBDIR = $2
-GODOT_RELEASE = $3
-
 GODOT_VERSION=${godot_version:-3.3.1}
+GODOT_DL_SUBDIR=${godot_subdir:-3.3.1}
+GODOT_RELEASE=${godot_release:-stable}
+
 while [ $# -gt 0 ]; do
    if [[ $1 == *"--"* ]]; then
         param="${1/--/}"
         declare $param="$2"
-        # echo $1 $2 // Optional to see the parameter:value result
    fi
   shift
 done
 
-echo $GODOT_VERSION 
+echo $GODOT_VERSION $GODOT_DL_SUBDIR $GODOT_RELEASE
 
 # GODOT_HEADLESS_BUILD = "https://downloads.tuxfamily.org/godotengine/3.3.1/Godot_v3.3.1-stable_linux_headless.64.zip"
 # GODOT_EXPORT_TEMPLATE = "https://downloads.tuxfamily.org/godotengine/3.3.1/Godot_v3.3.1-stable_export_templates.tpz"
