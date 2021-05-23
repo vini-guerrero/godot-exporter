a54035e7d6915d070524dd76d72332a2638f8569
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-23 16:46:06 -0300

    Create export.sh

---
+++ export.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "✔ Export script triggered successfully."
+
+
+mkdir -v -p ~/.local/share/godot/templates
+mv /root/.local/share/godot/templates/${GODOT_VERSION}.stable ~/.local/share/godot/templates/${GODOT_VERSION}.stable
+mkdir -v -p ~/.config/godot/
+cp /root/.config/godot/editor_settings-3.tres ~/.config/godot/editor_settings-3.tres
+echo 'export/android/debug_keystore = "/root/android-sdk/debug.keystore"' >> ~/.config/godot/editor_settings-3.tres
+echo 'export/android/android_sdk_path = "/root/android-sdk"' >> ~/.config/godot/editor_settings-3.tres
+
