3fdef618a1061881174d1639923f8df247a24f81
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-24 18:51:14 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -83,7 +83,7 @@ echo 'export/android/shutdown_adb_on_exit = true' >> ~/.config/godot/editor_sett
 
 echo -e Godot Editor .tres File Settings
 cat ~/.config/godot/editor_settings-3.tres
-cd $PLATFORM_TOOLS
+cd $ANDROID_SDK_ROOT
 pwd
 ls
 
