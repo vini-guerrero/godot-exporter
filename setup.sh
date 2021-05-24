4b4f70f11fc14438ff69cccf193a3a1f6102d1ea
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-23 22:15:21 -0300

    CI Test

--- setup.sh
+++ setup.sh
@@ -16,11 +16,9 @@ apt-get update && apt-get install -y --no-install-recommends \
     && rm -rf /var/lib/apt/lists/*
 
 
-GODOT_VERSION "3.3"
-GODOT_DL_SUBDIR "3.3"
-GODOT_RELEASE "stable"
-
-echo $GODOT_VERSION $GODOT_DL_SUBDIR $GODOT_RELEASE
+GODOT_VERSION = "3.3"
+GODOT_DL_SUBDIR = "3.3"
+GODOT_RELEASE = "stable"
 
 wget https://downloads.tuxfamily.org/godotengine/${GODOT_DL_SUBDIR}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip \
 && wget https://downloads.tuxfamily.org/godotengine/${GODOT_DL_SUBDIR}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz \
@@ -32,5 +30,7 @@ wget https://downloads.tuxfamily.org/godotengine/${GODOT_DL_SUBDIR}/Godot_v${GOD
 && unzip Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz \
 && mv templates/* ~/.local/share/godot/templates/${GODOT_VERSION}.${GODOT_RELEASE} \
 && rm -f Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip
+
+echo $GODOT_VERSION $GODOT_DL_SUBDIR $GODOT_RELEASE
 
 printenv
