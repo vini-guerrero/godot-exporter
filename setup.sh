adf85843fb674c3ad21e1cb53d6c049a68a47ba9
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-24 14:20:34 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@ GODOT_VERSION="3.3.1"
 GODOT_DL_SUBDIR="3.3.1"
 GODOT_RELEASE="stable"
 ANDROID_HOME="/root/android-sdk"
+GAME_PATH=$PWD
 
 # Download and install Godot Engine (headless) and export templates
 wget https://downloads.tuxfamily.org/godotengine/${GODOT_DL_SUBDIR}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip \
@@ -70,16 +71,16 @@ echo 'export/android/android_sdk_path = "/root/android-sdk"' >> ~/.config/godot/
 
 
 sudo mkdir -v -p ~/.local/share/godot/templates \
-&& sudo mv /root/.local/share/godot/templates/${GODOT_VERSION}.stable ~/.local/share/godot/templates/${GODOT_VERSION}.stable \
+&& sudo mv -p /root/.local/share/godot/templates/${GODOT_VERSION}.stable ~/.local/share/godot/templates/${GODOT_VERSION}.stable \
 && sudo mkdir -v -p ~/.config/godot/ \
 && sudo cp /root/.config/godot/editor_settings-3.tres ~/.config/godot/editor_settings-3.tres \
 
 echo 'export/android/debug_keystore = "/root/android-sdk/debug.keystore"' >> ~/.config/godot/editor_settings-3.tres
 echo 'export/android/android_sdk_path = "/root/android-sdk"' >> ~/.config/godot/editor_settings-3.tres
 
-cd /home/runner/work/ci_cd/ci_cd/game
+cd $GAME_PATH
 mkdir -v -p build/android
-./usr/local/bin/godot --verbose --export-debug "Android" ./build/android/$EXPORT_NAME.debug.apk
+#!/usr/bin/godot -s --verbose --export-debug "Android" ./build/android/$EXPORT_NAME.debug.apk
 
 echo -e Godot Engine Export Settings - Godot Version_${GODOT_VERSION} Subversion_${GODOT_DL_SUBDIR} Release_${GODOT_RELEASE}
 echo -e https://downloads.tuxfamily.org/godotengine/${GODOT_DL_SUBDIR}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip
