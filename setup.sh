f3c15d423f1a88d0b7dc0bf042e06f2a53d8ddc2
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-24 18:10:45 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,6 +23,9 @@ GODOT_RELEASE="stable"
 EXPORT_NAME="test"
 REPO_ROOT=$PWD
 ANDROID_HOME="/root/android-sdk"
+PLATFORM_TOOLS = $ANDROID_HOME/platform-tools/
+PATH=$PATH:$ANDROID_HOME/tools
+PATH=$PATH:$ANDROID_HOME/platform-tools
 
 # Download and install Godot Engine (headless) and export templates
 wget https://downloads.tuxfamily.org/godotengine/${GODOT_DL_SUBDIR}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip \
@@ -80,11 +83,6 @@ echo 'android/shutdown_adb_on_exit = true' >> ~/.config/godot/editor_settings-3.
 
 echo -e Godot Editor .tres File Settings
 cat ~/.config/godot/editor_settings-3.tres
-
-PLATFORM_TOOLS = $ANDROID_HOME/platform-tools/
-PATH=$PATH:$ANDROID_HOME/tools
-PATH=$PATH:$ANDROID_HOME/platform-tools
-
 cd $PLATFORM_TOOLS
 pwd
 ls
