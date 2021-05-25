0839ee0664e4f3711e5e4fca998b8c27172c2570
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-25 14:56:26 -0300

    Local Docker Update

--- setup.sh
+++ setup.sh
@@ -29,7 +29,7 @@ unzip Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz && sudo mv t
 # Clean
 sudo rm -f Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip
 
-echo "✔ Engine & Export Templates Successfully Installed."
+echo "\n\n ✔ Engine & Export Templates Successfully Installed. \n\n"
 
 # Android SDK
 sudo mkdir -p -v /root/android-sdk-installer/cmdline-tools && cd /root/android-sdk-installer/cmdline-tools
@@ -42,19 +42,19 @@ yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --licenses
 yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --sdk_root=$ANDROID_HOME "platform-tools" "build-tools;30.0.3" "platforms;android-29" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;21.4.7075529"
 cd /root && rm -rf /root/android-sdk-installer
 
-echo "✔ Android SDK Successfully Installed."
+echo "\n\n ✔ Android SDK Successfully Installed. \n\n"
 
 # Key Generation
 keytool -keyalg RSA -genkeypair -alias androiddebugkey -keypass android -keystore /root/android-sdk/debug.keystore -storepass android -dname "CN=Android Debug,O=Android,C=US" -validity 9999 
 ls /root/android-sdk/debug.keystore
 
-echo "✔ Debug Key Generated."
+echo "\n\n ✔ Debug Key Generated. \n\n"
 
 # Godot Executable From Path
 #!/usr/bin/godot
 chmod +x /usr/local/bin/godot && godot -q
 
-echo "✔ Godot Editor First Launch."
+echo "\n\n ✔ Godot Editor First Launch. \n\n"
 
 # Delete Default Editor Settings 
 sed -i '/export\/android\/adb/d' /root/.config/godot/editor_settings-3.tres \
@@ -73,10 +73,10 @@ sed -i '/\[resource\]/a export\/android\/adb = "/root/android-sdk/platform-tools
 && sed -i '/\[resource\]/a export\/android\/debug_user = "androiddebugkey"' /root/.config/godot/editor_settings-3.tres \
 && sed -i '/\[resource\]/a export\/android\/debug_pass = "android"' /root/.config/godot/editor_settings-3.tres
 
-echo "✔ Exporting Android Project"
+echo "\n\n ✔ Exporting Android Project \n\n"
 
 cat /root/.config/godot/editor_settings-3.tres
 cd /game && mkdir -v -p build/android
 godot --verbose --export-debug "Android" build/android/$EXPORT_NAME.debug.apk
 
-echo "✔ Android Project Exported At /game/build/android"
+echo "\n\n ✔ Android Project Exported At /game/build/android \n\n"
