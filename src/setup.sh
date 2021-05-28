5b83ceb5a2b5520699ad977d798afd215a85cd85
Andrea-Miele <72763162+Andrea-Miele@users.noreply.github.com>
2021-05-28 19:06:19 +0200

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -97,11 +97,11 @@ do
     echo "✔ Exporting ${platform} Platform"
     mkdir -v -p "build/${platform}"
     
-    if [[ $platform == "Linux/X11" ]]
+    if [[ $platform == "Linux" ]]
     then                
         godot --verbose --export "${platform}" "build/${platform}/${EXPORT_NAME}.x86_64"
         zip -r "${platform}.zip" build/${platform}
-        ACTIONS_RUNTIME_TOKEN=$ACTIONS_RUNTIME_TOKEN NAME="Linux X11" FILES="${platform}.zip" ROOT_DIR="${GITHUB_WORKSPACE}" node ${GITHUB_WORKSPACE}/upload_artifacts/index.js
+        ACTIONS_RUNTIME_TOKEN=$ACTIONS_RUNTIME_TOKEN NAME="Linux" FILES="${platform}.zip" ROOT_DIR="${GITHUB_WORKSPACE}" node ${GITHUB_WORKSPACE}/upload_artifacts/index.js
     elif [[ $platform == "Mac OSX" ]]
     then
         godot --verbose --export "${platform}" "build/${platform}/${EXPORT_NAME}.zip"
