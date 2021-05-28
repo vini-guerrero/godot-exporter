e9dea0d51f5c5232903e6d41ac4c7263c2fa5ebb
Andrea-Miele <72763162+Andrea-Miele@users.noreply.github.com>
2021-05-28 12:32:39 +0200

    Update index.js

--- upload_artifacts/index.js
+++ upload_artifacts/index.js
@@ -2,7 +2,7 @@ const artifact = require('@actions/artifact');
 const artifactClient = artifact.create()
 const artifactName = 'my-artifact';
 const files = [
-    '/github/workflow/artifact.zip'
+    '/github/workspace/artifact.zip'
 ]
 const rootDirectory = '/github/workspace/'
 const options = {
