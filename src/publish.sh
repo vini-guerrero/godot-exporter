644d4a50e1e14057c8a37275d6da1d4ebd036fde
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-05-28 14:44:58 -0300

    Updated Readme

---
+++ src/publish.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+echo "\n\n ✔ Publish Script Triggered Successfully. \n\n "
+
+# Itch.io - Butler Integration
+curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default \
+&& unzip butler.zip \
+&& cp butler /usr/bin \
+&& chmod +x /usr/bin/butler
