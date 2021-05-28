73e1a68492e0ddac7d0462bc2f9c56d2b326d465
Andrea Miele <andrea.miele08@gmail.com>
2021-05-28 08:52:31 +0000

    init commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:20.04
+FROM ubuntu:20.04 AS exporter
 
 LABEL author="Vinicius Guerrero"
 
@@ -15,4 +15,10 @@ COPY . .
 
 RUN chmod +x ./src/setup.sh
 
-ENTRYPOINT ["bash", "./src/setup.sh"]
+RUN bash ./src/setup.sh
+
+FROM node:12
+COPY --from=exporter artifact.zip .
+COPY upload_artifacts .
+RUN npm install
+ENTRYPOINT node index.js
---
+++ upload_artifacts/index.js
@@ -0,0 +1,12 @@
+const artifact = require('@actions/artifact');
+const artifactClient = artifact.create()
+const artifactName = 'my-artifact';
+const files = [
+    'artifact.zip'
+]
+const rootDirectory = '.'
+const options = {
+    continueOnError: true
+}
+
+const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
