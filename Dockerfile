dd2eb8f4599e90d214b52b2a6e872bc762e07c4a
Andrea-Miele <72763162+Andrea-Miele@users.noreply.github.com>
2021-05-31 20:17:39 +0200

    Some fixes (#8)
    Co-authored-by: Vinicius Guerrero <vini.ts.guerrero@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,13 @@
+# Local Docker Image
 FROM ubuntu:20.04
-
 ENV DEBIAN_FRONTEND=noninteractive
-
 COPY . .
-
-# RUN apt-get update && apt-get install -y --no-install-recommends sudo ca-certificates git python python-openssl unzip wget zip curl openjdk-8-jdk apksigner nano curl dirmngr apt-transport-https lsb-release ca-certificates
-
-# RUN curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-
-# RUN sudo apt -y install nodejs 
-
+RUN chmod +x /src/install.sh && bash /src/install.sh
 RUN chmod +x /src/setup.sh
-
 ENTRYPOINT ["bash", "/src/setup.sh"]
 
+# Docker Hub Image
+# FROM viiniiguerrero/godot-exporter
+# RUN chmod +x /src/install.sh && bash /src/install.sh
+# RUN chmod +x /src/setup.sh
+# ENTRYPOINT ["bash", "/src/setup.sh"]
---
+++ src/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -e
+echo "\n\n ✔ Install Script Triggered Successfully. \n\n "
+
+
+ANDROID_HOME="/root/android-sdk"
+
+# Download and Install Packages
+apt-get update && apt-get install -y --no-install-recommends sudo ca-certificates git python python-openssl unzip wget zip curl openjdk-8-jdk apksigner nano curl dirmngr apt-transport-https lsb-release ca-certificates graphicsmagick
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt -y install nodejs 
+# locales-all
+rm -rf /var/lib/apt/lists/*
+
+
+# Android SDK Export Dependencies
+sudo mkdir -p -v /root/android-sdk-installer/cmdline-tools && cd /root/android-sdk-installer/cmdline-tools
+wget "https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip"
+# curl -fsSLO "https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip"
+unzip -q commandlinetools-linux-*.zip
+rm commandlinetools-linux-*.zip
+mv cmdline-tools latest
+mkdir -p -v /root/.android && echo "count=0" > /root/.android/repositories.cfg
+yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --licenses
+yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --sdk_root=$ANDROID_HOME "platform-tools" "build-tools;30.0.3" "platforms;android-29" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;21.4.7075529"
+cd /root && rm -rf /root/android-sdk-installer    
+echo "✔ Android SDK Successfully Installed."
+
+# Key Generation
+keytool -keyalg RSA -genkeypair -alias androiddebugkey -keypass android -keystore /root/android-sdk/debug.keystore -storepass android -dname "CN=Android Debug,O=Android,C=US" -validity 9999 
+ls /root/android-sdk/debug.keystore
+echo "✔ Debug Key Generated."
+
+
+# Itch.io - Butler Integration
+curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default \
+&& unzip butler.zip \
+&& cp butler /usr/bin \
+&& chmod +x /usr/bin/butler
