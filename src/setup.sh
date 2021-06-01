571d26374dfaff16a8a7b595b16683d4fb7330ad
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-06-01 17:22:12 -0300

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -74,7 +74,7 @@ then
     && sed -i '/\required_icons/\app_store_1024x1024="${ICON_PATH}"' /github/workspace/${EXPORT_PATH}/export_presets.cfg \
     && sed -i '/\required_icons/\iphone_180x180="${ICON_PATH}"' /github/workspace/${EXPORT_PATH}/export_presets.cfg \
     && sed -i '/\required_icons/\ipad_152x152="${ICON_PATH}"' /github/workspace/${EXPORT_PATH}/export_presets.cfg \
-    && sed -i '/\required_icons/\ipad_167x167="${ICON_PATH}"' /github/workspace/${EXPORT_PATH}/export_presets.cfg \  
+    && sed -i '/\required_icons/\ipad_167x167="${ICON_PATH}"' /github/workspace/${EXPORT_PATH}/export_presets.cfg \
     && sed -i '/\required_icons/\spotlight_40x40="${ICON_PATH}"' /github/workspace/${EXPORT_PATH}/export_presets.cfg \
     && sed -i '/\required_icons/\spotlight_80x80="${ICON_PATH}"' /github/workspace/${EXPORT_PATH}/export_presets.cfg
     echo "✔ iOS Project Export Setup Ready"
