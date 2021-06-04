0fb1708848fc76ed7e058b030b1863171dbafaad
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-06-04 16:34:48 -0300

    Update ios_icons.sh

--- src/ios_icons.sh
+++ src/ios_icons.sh
@@ -68,4 +68,5 @@ generate_size $((98 * 2))  "${output_file_prefix}98@2x.png"
 WORKSPACE_PATH="${GITHUB_WORKSPACE}"
 ARTIFACT_NAME="Icons"
 zip -r ${ARTIFACT_NAME}.zip ${icons_folder} && ls && pwd
+EXPORT ACTION_RUNTIME_TOKEN=$ACTION_RUNTIME_TOKEN
 ACTION_RUNTIME_TOKEN=$ACTION_RUNTIME_TOKEN NAME="${ARTIFACT_NAME}" FILES="${ARTIFACT_NAME}.zip" ROOT_DIR="${WORKSPACE_PATH}" node upload_artifacts/index.js
