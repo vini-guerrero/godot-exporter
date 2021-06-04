db55b496b936836eea7f1f186122dc32da11d84a
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-06-04 14:16:27 -0300

    iOS Build Action Testing

--- src/ios_icons.sh
+++ src/ios_icons.sh
@@ -22,7 +22,7 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-apt-get update && apt-get install -y --no-install-recommends sudo graphicsmagick
+sudo apt-get update && apt-get install -y --no-install-recommends sudo graphicsmagick
 
 source_file=$1
 output_file_prefix=app_icon_
