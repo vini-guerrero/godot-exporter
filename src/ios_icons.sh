43644306967a8f82a18810b08ebb943cd60fbe81
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-06-04 14:56:06 -0300

    iOS Build Action Testing

--- src/ios_icons.sh
+++ src/ios_icons.sh
@@ -63,4 +63,4 @@ generate_size $((98 * 2))  "${output_file_prefix}98@2x.png"
 
 WORKSPACE_PATH="${WORKSPACE_PATH:="/github/workspace"}"
 zip -r icons.zip ${icons_folder} && ls && pwd
-ACTIONS_RUNTIME_TOKEN=$ACTIONS_RUNTIME_TOKEN NAME="Icons" FILES="icons.zip" ROOT_DIR="${WORKSPACE_PATH}/" node /upload_artifacts/index.js
+ACTIONS_RUNTIME_TOKEN=$ACTIONS_RUNTIME_TOKEN NAME="Icons" FILES="icons.zip" ROOT_DIR="${WORKSPACE_PATH}/" node upload_artifacts/index.js
