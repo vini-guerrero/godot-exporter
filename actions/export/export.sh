c9abcb98be6b432944194e5f7ab22409013c15b0
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-19 18:19:28 -0300

    Update export.sh

--- actions/export/export.sh
+++ actions/export/export.sh
@@ -5,7 +5,7 @@ echo -e "✔ Export Script Triggered Successfully."
 
 # Install Export Dependencies
 # sudo apt-get update
-sudo apt-get install -y -qq apksigner locales
+sudo apt-get install -y -qq locales # apksigner
 sudo sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sudo dpkg-reconfigure --frontend=noninteractive locales
 sudo update-locale LANG=en_US.UTF-8
