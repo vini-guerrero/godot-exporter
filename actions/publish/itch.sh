a19db48777393af59deec9b45bb1ca302dd5cede
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-20 13:49:39 -0300

    Itch.Io Publishing

--- actions/publish/itch.sh
+++ actions/publish/itch.sh
@@ -6,7 +6,7 @@ echo -e "✔ Publish Script Triggered Successfully."
 sudo curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
 sudo unzip butler.zip && sudo cp butler /usr/bin && sudo chmod 777 /usr/bin/butler
 
-export BUTLER_API_KEY=$BUTLER_CREDENTIALS
+export BUTLER_API_KEY=$ITCH_CREDENTIALS
 versionArgument=""
 
 if [ "$VERSION" != "" ]
