b09a9ff9d0b3fcd17d9449fac0f3786795f905f3
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-20 12:53:50 -0300

    Update publish.sh

--- actions/publish/publish.sh
+++ actions/publish/publish.sh
@@ -4,4 +4,4 @@ set -e
 echo -e "✔ Publish Script Triggered Successfully."
 
 sudo curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
-sudo unzip butler.zip && sudo chmod 777 butler
+sudo unzip butler.zip && sudo cp butler /usr/bin && sudo chmod 777 /usr/bin/butler
