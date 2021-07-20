71072218f6ee2d54bcfa1a432d28919861552942
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-20 18:57:27 -0300

    Update itch.sh

--- actions/publish/itch.sh
+++ actions/publish/itch.sh
@@ -3,20 +3,6 @@ set -e
 
 echo -e "✔ Publish Script Triggered Successfully."
 
-sudo curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
-sudo unzip butler.zip && sudo cp butler /usr/bin && sudo chmod 777 /usr/bin/butler
-
-export BUTLER_API_KEY=$ITCH_CREDENTIALS
-versionArgument=""
-
-if [ "$VERSION" != "" ]
-then
-    versionArgument="--userversion ${VERSION}"
-elif [ "$VERSION_FILE" != "" ]
-then
-    versionArgument="--userversion-file ${VERSION_FILE}"
-fi
-
 PACKAGE=$1
 CHANNEL="None"
 declare -a SUPPORTED_CHANNELS=("Linux" "MacOS" "Windows", "Android")
@@ -28,6 +14,17 @@ if [ "$2" == "Android" ]; then CHANNEL=android; fi
 
 if [ "$CHANNEL" != "None" ]; 
 then
+    sudo curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
+    sudo unzip butler.zip && sudo cp butler /usr/bin && sudo chmod 777 /usr/bin/butler
+    
+    export BUTLER_API_KEY=$ITCH_CREDENTIALS
+    versionArgument=""
+    if [ "$VERSION" != "" ]; then
+        versionArgument="--userversion ${VERSION}"
+    elif [ "$VERSION_FILE" != "" ]; then
+        versionArgument="--userversion-file ${VERSION_FILE}"
+    fi
+
     if [[ ${SUPPORTED_CHANNELS[@]} =~ "$2" ]]; then 
         echo -e "\n ✔ Upload $1 To Itch.io On Channel ${CHANNEL} \n"
         echo "butler push \"$PACKAGE\" $ITCH_USER/$ITCH_GAME:$CHANNEL ${versionArgument}"
