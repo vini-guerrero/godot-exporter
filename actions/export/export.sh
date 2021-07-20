2b2c4f9ce0052299aacca614ab08950d49475603
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-20 15:58:38 -0300

    Update export.sh

--- actions/export/export.sh
+++ actions/export/export.sh
@@ -13,6 +13,7 @@ LANG=en_US.UTF-8
 
 # Environment Variables
 EXPORT_PLATFORM=$1
+INITIAL_PATH=$(eval "pwd")
 PROJECT_PATH="${PROJECT_PATH:="game"}"
 GODOT_PATH="${GODOT_PATH:="/usr/local/bin"}"
 GODOT_RELEASE="${GODOT_RELEASE:="stable"}"
@@ -67,16 +68,17 @@ fi
 if [[ "$EXPORT_PLATFORM" == "iOS" ]]; then 
     # Set Editor Settings For iOS Export
     ICON_PATH="res:\/\/assets\/sprites\/icon\.png"
-    sudo sed -i '/\[rendering\]/a vram_compression\/import_pvrtc=true' ${GITHUB_WORKSPACE}/${PROJECT_PATH}/project.godot \
-    && sudo sed -i 's@required_icons/iphone_120x120[[:space:]]*=[[:space:]]*".*"@required_icons/iphone_120x120 = "'${ICON_PATH}'"@g' ${GITHUB_WORKSPACE}/${PROJECT_PATH}/export_presets.cfg \
-    && sudo sed -i 's@required_icons/ipad_76x76[[:space:]]*=[[:space:]]*".*"@required_icons/ipad_76x76 = "'${ICON_PATH}'"@g' ${GITHUB_WORKSPACE}/${PROJECT_PATH}/export_presets.cfg \
-    && sudo sed -i 's@required_icons/app_store_1024x1024[[:space:]]*=[[:space:]]*".*"@required_icons/app_store_1024x1024 = "'${ICON_PATH}'"@g' ${GITHUB_WORKSPACE}/${PROJECT_PATH}/export_presets.cfg \
+    sudo sed -i '/\[rendering\]/a vram_compression\/import_pvrtc=true' ${INITIAL_PATH}/${PROJECT_PATH}/project.godot \
+    && sudo sed -i 's@required_icons/iphone_120x120[[:space:]]*=[[:space:]]*".*"@required_icons/iphone_120x120 = "'${ICON_PATH}'"@g' ${INITIAL_PATH}/${PROJECT_PATH}/export_presets.cfg \
+    && sudo sed -i 's@required_icons/ipad_76x76[[:space:]]*=[[:space:]]*".*"@required_icons/ipad_76x76 = "'${ICON_PATH}'"@g' ${INITIAL_PATH}/${PROJECT_PATH}/export_presets.cfg \
+    && sudo sed -i 's@required_icons/app_store_1024x1024[[:space:]]*=[[:space:]]*".*"@required_icons/app_store_1024x1024 = "'${ICON_PATH}'"@g' ${INITIAL_PATH}/${PROJECT_PATH}/export_presets.cfg \
     echo "✔ iOS Project Export Setup Ready"
 fi
 
 # Validate Editor Settings
+echo -e ${INITIAL_PATH}
 sudo cat ${TRES_PATH} 
-sudo cat ${GITHUB_WORKSPACE}/${PROJECT_PATH}/export_presets.cfg
+sudo cat ${INITIAL_PATH}/${PROJECT_PATH}/export_presets.cfg
 echo -e "✔ Export Path."
 cd ${PROJECT_PATH} && mkdir -v -p "build/${EXPORT_PLATFORM}"
 
