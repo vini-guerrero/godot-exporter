f3f155efdfe19f29c74f0a4366c50f41f603a41c
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-21 17:20:15 -0300

    Update export.sh

--- actions/export/export.sh
+++ actions/export/export.sh
@@ -119,7 +119,7 @@ elif [[ "${EXPORT_PLATFORM}" == "Android" ]]; then
 fi
 
 EXPORT_NAME="${PROJECT_NAME}${GAME_EXTENSION}"
-export EXPORT_PATH=${PROJECT_REPO_PATH}/build/${EXPORT_PLATFORM}/${EXPORT_NAME}
+EXPORT_PATH=${PROJECT_REPO_PATH}/build/${EXPORT_PLATFORM}/${EXPORT_NAME}
 
 echo -e "✔ Exporting ${EXPORT_PLATFORM} Version."
 if [ "$EXPORT_DEBUG" == "true" ]; then 
