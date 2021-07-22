b1609c91bfc1599f60338d8d687810cfb185dc21
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-22 11:56:04 -0300

    Uploader

--- actions/export/index.js
+++
@@ -1,3 +0,0 @@
-console.log(`::set-env name=ACTIONS_RUNTIME_URL::${process.env.ACTIONS_RUNTIME_URL}`)
-console.log(`::set-env name=ACTIONS_RUNTIME_TOKEN::${process.env.ACTIONS_RUNTIME_TOKEN}`)
-console.log(`::set-env name=GITHUB_RUN_ID::${process.env.GITHUB_RUN_ID}`)
