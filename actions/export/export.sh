feacdcc8ac8a4f831ec86c4d49bbd67b6fc5928e
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-22 01:33:47 -0300

    Update export.sh

--- actions/export/export.sh
+++ actions/export/export.sh
@@ -79,8 +79,7 @@ fi
 
 
 # Validate Editor Settings
-ls -l && pwd
-zip -rj ${PROJECT_REPO_PATH}/export_settings.zip ${PROJECT_REPO_PATH}/export_presets.cfg # ${TRES_PATH}
+zip -rj ${PROJECT_REPO_PATH}/export_settings.zip ${PROJECT_REPO_PATH}/export_presets.cfg ${TRES_PATH}
 
 echo -e "✔ Export Path."
 mkdir -v -p "${PROJECT_REPO_PATH}/build/${EXPORT_PLATFORM}" 
