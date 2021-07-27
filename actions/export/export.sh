552094188968783fdc7fca36e76f0d52c21b98f1
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-27 10:43:03 -0300

    Release Mode Android

--- actions/export/export.sh
+++ actions/export/export.sh
@@ -66,7 +66,7 @@ if [[ "$EXPORT_PLATFORM" == "Android" ]]; then
     # Prepare Release Mode
     if [ "$EXPORT_MODE" == "release" ]; then 
         echo ${K8S_SECRET_RELEASE_KEYSTORE_BASE64} | base64 --decode > /root/release.keystore 
-        && sudo sed 's@keystore/release[[:space:]]*=[[:space:]]*".*"@keystore/release = "/root/release.keystore"@g' -i ${PROJECT_REPO_PATH}/export_presets.cfg \
+        sudo sed 's@keystore/release[[:space:]]*=[[:space:]]*".*"@keystore/release = "/root/release.keystore"@g' -i ${PROJECT_REPO_PATH}/export_presets.cfg \
         && sudo sed 's@keystore/release_user[[:space:]]*=[[:space:]]*".*"@keystore/release_user="'${K8S_SECRET_RELEASE_KEYSTORE_USER}'"@g' -i ${PROJECT_REPO_PATH}/export_presets.cfg \
         && sudo sed 's@keystore/release_password[[:space:]]*=[[:space:]]*".*"@keystore/release_password="'${K8S_SECRET_RELEASE_KEYSTORE_PASSWORD}'"@g' -i ${PROJECT_REPO_PATH}/export_presets.cfg
     fi
